4bb5656326848fb1034f14f9eaafadbfbb9ff1d6
Seb <sl767@cam.ac.uk>
2018-07-03 22:53:09 +0100

    training forward model - bug fix

--- training.py
+++ training.py
@@ -2,6 +2,6 @@ import Framework as fr
 
 frame = fr.framework()
 batch_size = 64
-learning_rate = 0.001
+learning_rate = 0.0001
 for k in range(20):
-    frame.train_correction(200, batch_size=batch_size, learning_rate=learning_rate)
+    frame.train_correction(2000, batch_size=batch_size, learning_rate=learning_rate)
