4ff531dbbd3717363f51cb568ad84be65629936f
Seb <sl767@cam.ac.uk>
2018-07-19 12:14:46 +0100

    concatenation introduced

--- Framework.py
+++ Framework.py
@@ -221,8 +221,8 @@ class model_correction(np_operator):
             result = result[0,...]
         return result
 
-    def differentiate(self, location, direction):
-        location, change = self.feedable_format(location)
+    def differentiate(self, point, direction):
+        location, change = self.feedable_format(point)
         direction, _  =self.feedable_format(direction)
         result = self.sess.run(self.gradients, feed_dict={self.approximate_y: location, self.direction: direction})[...,0]
         if change:
