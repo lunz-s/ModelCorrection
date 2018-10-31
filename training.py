db3dff9185414ee7b21ae3b540cd23a6f277db57
Seb <sl767@cam.ac.uk>
2018-10-31 18:03:29 +0000

    bug fix odl.as_tf_layer

--- training.py
+++ training.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 frame = fr.framework_regularised()
 batch_size = 64
-learning_rate = 0.0001
+learning_rate = 0.001
 for k in range(20):
-    rate = learning_rate/k
+    rate = learning_rate/(k+3)
     frame.train_correction(2000, batch_size=batch_size, learning_rate=rate)
 
 ###
