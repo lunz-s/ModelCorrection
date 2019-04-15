c8b405337f94dd64cc855d6c36199452094fc363
lunz-s <sl767@cam.ac.uk>
2019-04-15 15:40:47 +0300

    two nets updated

--- Adjoint_network.py
+++ Adjoint_network.py
@@ -6,6 +6,9 @@ from Operators.networks import UNet
 # Unlike in the other implementations of the correction opertor,
 # here the correction operator already includes the approximate forward operator!
 
+def l2(tensor):
+    return tf.reduce_mean(tf.sqrt(tf.reduce_sum(tf.square(tensor), axis=(1, 2, 3))))
+
 
 class TwoNets(model_correction):
     linear = False
@@ -29,10 +32,18 @@ class TwoNets(model_correction):
         self.m_appr = tf.constant(self.appr_op.m, dtype=tf.float32)
 
         def multiply(tensor, matrix):
+            tensor_flipped = tf.reverse(tensor, axis=[1])
+            shape = tf.shape(tensor)
+            reshaped = tf.reshape(tensor_flipped, [-1, shape[1]*shape[2], 1])
+            result = tf.tensordot(reshaped, matrix, axes=[[1], [1]])
+            return tf.reshape(result, [-1, shape[1], shape[2], 1])
+
+        def multiply_adjoint(tensor, matrix):
             shape = tf.shape(tensor)
             reshaped = tf.reshape(tensor, [-1, shape[1]*shape[2], 1])
-            result = tf.tensordot(reshaped, matrix, axes=[[1], [0]])
-            return tf.reshape(result, [-1, shape[1], shape[2], 1])
+            prod = tf.tensordot(reshaped, tf.transpose(matrix), axes=[[1], [1]])
+            flipped = tf.reverse(tf.reshape(prod, [-1, shape[1], shape[2], 1]), axis=[1])
+            return flipped
 
         ### The forward network ###
 
@@ -43,7 +54,7 @@ class TwoNets(model_correction):
         self.data_term = tf.placeholder(shape=[None, self.measurement_size[0], self.measurement_size[1], 1], dtype=tf.float32)
 
         # methode to get the initial guess in tf
-        self.x_ini = multiply(self.data_term, tf.transpose(self.m_true))
+        self.x_ini = multiply_adjoint(self.data_term, self.m_true)
 
         # Compute the corresponding measurements with the true and approximate operators
         self.true_y = multiply(self.input_image, self.m_true)
@@ -56,8 +67,7 @@ class TwoNets(model_correction):
             self.output = self.UNet.net(self.approximate_y)
 
             # forward loss
-            loss = tf.sqrt(tf.reduce_sum(tf.square(self.output - self.true_y), axis=(1,2,3)))
-            self.l2 = tf.reduce_mean(loss)
+            self.l2 = l2(self.output - self.true_y)
 
             # optimization algorithm
             self.global_step = tf.Variable(0, name='Step', trainable=False)
@@ -65,16 +75,15 @@ class TwoNets(model_correction):
                                                                                  global_step=self.global_step)
 
             # Direction the adjoint correction is calculated in
-            self.direction = self.UNet.net(self.true_y) - self.true_y
+            self.direction = self.output - self.true_y
             direction = tf.stop_gradient(self.direction)
 
-        # some tensorboard logging
-        tf.summary.image('TrueData', self.true_y, max_outputs=1)
-        tf.summary.image('ApprData', self.approximate_y, max_outputs=1)
-        tf.summary.image('NetworkData', self.output, max_outputs=1)
-        tf.summary.scalar('Loss_L2', self.l2)
+            # some tensorboard logging
+            tf.summary.image('TrueData', self.true_y, max_outputs=1)
+            tf.summary.image('ApprData', self.approximate_y, max_outputs=1)
+            tf.summary.image('NetworkData', self.output, max_outputs=1)
+            tf.summary.scalar('Loss_L2', self.l2)
 
-        # placeholders
         self.approximate_x = multiply(direction, tf.transpose(self.m_appr))
         self.true_x = multiply(direction, tf.transpose(self.m_true))
 
@@ -82,21 +91,27 @@ class TwoNets(model_correction):
             self.correct_adj = self.UNet.net(self.approximate_x)
 
             # forward loss
-            loss = tf.sqrt(tf.reduce_sum(tf.square(self.correct_adj - self.true_x), axis=(1,2,3)))
-            self.l2_adj = tf.reduce_mean(loss)
+            self.l2_adj = l2(self.correct_adj - self.true_x)
 
             # optimization algorithm
             self.step_adjoint = tf.Variable(0, name='Step', trainable=False)
             self.optimizer_adjoint = tf.train.AdamOptimizer(self.learning_rate).minimize(self.l2_adj,
                                                                                  global_step=self.step_adjoint)
-        # some tensorboard logging
-        tf.summary.image('TrueAdjoint', self.true_x, max_outputs=1)
-        tf.summary.image('ApprAdjoint', self.approximate_x, max_outputs=1)
-        tf.summary.image('NetworkAdjoint', self.correct_adj, max_outputs=1)
-        tf.summary.scalar('Loss_Adjoint', self.l2_adj)
+            # some tensorboard logging
+            tf.summary.image('TrueAdjoint', self.true_x, max_outputs=1)
+            tf.summary.image('ApprAdjoint', self.approximate_x, max_outputs=1)
+            tf.summary.image('NetworkAdjoint', self.correct_adj, max_outputs=1)
+            tf.summary.scalar('Loss_Adjoint', self.l2_adj)
 
         self.merged = tf.summary.merge_all()
         self.writer = tf.summary.FileWriter(self.path + 'Logs/', self.sess.graph)
+
+        self.ground_truth = tf.placeholder(shape=[None, self.image_size[0], self.image_size[1], 1], dtype=tf.float32)
+        self.quality = l2(self.input_image - self.ground_truth)
+        tf.summary.scalar('Quality', self.quality)
+        tf.summary.scalar('DataTermTrueOP', self.l2(self.true_y - self.data_term))
+        tf.summary.scalar('DataTermCorrectedOP', self.l2(self.output - self.data_term))
+        self.merged_gd = tf.summary.merge_all()
 
         # initialize variables
         tf.global_variables_initializer().run()
@@ -110,23 +125,15 @@ class TwoNets(model_correction):
     def differentiate(self, point, direction):
         pass
 
-    def train_forward(self, learning_rate):
+    def train(self, recursion, steps_size, learning_rate):
         appr, true, image = self.data_sets.train.next_batch(self.batch_size)
         x = self.sess.run(self.x_ini, feed_dict={self.data_term: true})
 
         self.sess.run(self.optimizer, feed_dict={self.input_image: x, self.data_term: true,
                                                  self.learning_rate: learning_rate})
 
-    def train_adjoint(self, learning_rate):
-        appr, true, image = self.data_sets.train.next_batch(self.batch_size)
-        x = self.sess.run(self.x_ini, feed_dict={self.data_term: true})
-
         self.sess.run(self.optimizer_adjoint, feed_dict={self.input_image: x, self.data_term: true,
                                                          self.learning_rate: learning_rate})
-
-    def train(self, recursion, steps_size, learning_rate):
-        self.train_forward(learning_rate)
-        self.train_adjoint(learning_rate=learning_rate)
 
     def log(self, recursions, steps_size):
         appr, true, image = self.data_sets.test.next_batch(self.batch_size)
@@ -135,3 +142,16 @@ class TwoNets(model_correction):
         iteration, summary = self.sess.run([self.global_step, self.merged],
                     feed_dict={self.input_image: x, self.data_term: true})
         self.writer.add_summary(summary, iteration)
+
+    def log_optimization(self, recursions, step_size):
+        appr, true, image = self.data_sets.test.next_batch(self.batch_size)
+        step, x = self.sess.run([self.global_step, self.x_ini], feed_dict={self.data_term: true})
+        writer = tf.summary.FileWriter(self.path + 'Logs/Iteration_' + str(step)+'/')
+        for k in range(recursions):
+            summary, update = self.sess.run([self.merged_gd, self.correct_adj],
+                               feed_dict={self.input_image: x, self.data_term: true,
+                                          self.ground_truth: image})
+            writer.add_summary(summary, k)
+            x = x-2*step_size*update
+        writer.flush()
+        writer.close()
---
+++ training_TwoNets.py
@@ -0,0 +1,54 @@
+from Adjoint_network import TwoNets
+import Operators.Load_PAT2D_data as PATdata
+import platform
+from Framework import approx_PAT_matrix as ApproxPAT
+from Framework import exact_PAT_operator as ExactPAT
+
+if platform.node() == 'motel':
+    prefix = '/local/scratch/public/sl767/ModelCorrection/'
+else:
+    prefix = ''
+
+matrix_path = prefix+'Data/Matrices/threshSingleMatrix4Py.mat'
+data_path = prefix+'Data/balls64/'
+saves_path = prefix+'Saves/balls64/'
+
+print(saves_path)
+print(data_path)
+
+train_append = 'trainDataSet.mat'
+test_append = 'testDataSet.mat'
+data_sets = PATdata.read_data_sets(data_path + train_append, data_path + test_append)
+
+input_dim = data_sets.train.image_resolution
+output_dim = data_sets.train.y_resolution
+
+approx = ApproxPAT(matrix_path=matrix_path, input_dim=input_dim, output_dim=output_dim)
+exact = ExactPAT(matrix_path=matrix_path, input_dim=input_dim, output_dim=output_dim)
+
+
+correction = TwoNets(path=saves_path, true_np=exact, appr_np=approx, data_sets=data_sets)
+
+rate = 5e-4
+recursions = 1
+step_size = 0.1
+iterations = 5
+
+for i in range(iterations):
+    for k in range(1000):
+        correction.train(recursions, step_size, learning_rate=rate)
+        if k % 50 == 0:
+            correction.log(recursions, step_size)
+    # recursions = recursions+1
+    correction.save()
+correction.log_optimization(recursions=40, step_size=step_size)
+
+# for i in range(iterations):
+#     for k in range(1000):
+#         correction.train(recursions, step_size, rate/10.0)
+#         if k % 50 == 0:
+#             correction.log(recursions, step_size)
+#     correction.save()
+# correction.log_optimization(recursions=10, step_size=step_size)
+
+correction.end()
