beaf880e4437c84df930d404cda83f62f929c45d
lunz-s <sl767@cam.ac.uk>
2019-05-30 12:10:05 +0100

    augmentation added

--- ut.py
+++ ut.py
@@ -4,15 +4,26 @@ import numpy as np
 import random as ran
 
 
+def ident(batch):
+    return batch
+
+def flipud(batch):
+    res = np.zeros(shape=batch.shape)
+    for k in range(batch.shape[0]):
+        res[k,...,0] = np.flipud[k,...,0]
+    return res
+
+def fliplr(batch):
+    res = np.zeros(shape=batch.shape)
+    for k in range(batch.shape[0]):
+        res[k,...,0] = np.fliplr[k,...,0]
+    return res
+
 def augmentation(batch):
     sigma = ran.uniform(0,4)
     aug = edf.deform_random_grid(batch, axis=(1, 2), sigma=sigma, points=4)
-    up = [id, np.flipud]
-    lr = [id, np.fliplr]
+    up = [ident, flipud]
+    lr = [ident, fliplr]
     aug = ran.choice(up)(aug)
     aug = ran.choice(lr)(aug)
     return aug
-
-
-def id(batch):
-    return batch
